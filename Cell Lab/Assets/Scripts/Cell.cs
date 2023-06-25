c5490f075ff0f99131b95cd30558075095fddab1
edy131109 <lazareduard702@gmail.com>
2023-06-25 22:19:37 +0300

    Cell Physics
    Added Velocity, Wall Collisions and Cell Death if outside wall bounds.

--- Cell Lab/Assets/Scripts/Cell.cs
+++ Cell Lab/Assets/Scripts/Cell.cs
@@ -1,38 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Cell : MonoBehaviour
 {
-    public Vector2 position = new Vector2(0, 0);
-    public Vector2 velocity = new Vector2(5000000, 0);
+    const float springConst = 2000;
     public GameObject cellObject;
     public Rigidbody2D rb;
     public Substrate substrate;
+    public float radius = 30;
+    public bool dead = false;
     // Start is called before the first frame update
     void Start()
     {
         cellObject = this.gameObject;
-        rb = cellObject.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(0, 0);
-        velocity.x = 0;
-        velocity.y = 0;
+        cellObject.transform.localScale = new Vector3(radius, radius, 1);
+        rb = cellObject.AddComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+        rb.velocity = new Vector2(0.1f, 0.1f);
+        cellObject.GetComponent<SpriteRenderer>().color = (Color)(new Color32(179, 255, 57, 255));
     }
 
     public void update()
     {
-        if (rb.velocity.x <= velocity.x || rb.velocity.y <= velocity.y) //fix later
+        cellObject.transform.localScale = new Vector3(radius, radius, 1);
+        Vector2 position = GetPos();
+        float length = position.magnitude;
+        if (length >= 1)
         {
+            Kill();
+            return;
         }
     }
 
     public void fixedupdate()
     {
-        
+        Vector2 position = GetPos();
+        float r = radius / 1000;
+        float length = position.magnitude;
+        float substrateCollision = length + r - 1;
+        if (substrateCollision > 0)
+        {
+            Vector2 force = -position / length * substrateCollision * springConst;
+            //force.x = Mathf.Round(force.x);
+            //force.y = Mathf.Round(force.y);
+            //Debug.Log(force);
+            rb.AddRelativeForce(force);
+        }
+    }
+
+    public Vector2 GetPos()
+    {
+        return this.gameObject.transform.position;
     }
 
     public void Kill()
     {
-        Destroy(cellObject);
+        dead = true;
+    }
+
+    public void Destroy()
+    {
+        Destroy(this.gameObject);
     }
 }
+
--- Cell Lab/Assets/Scripts/Substrate.cs
+++ Cell Lab/Assets/Scripts/Substrate.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Substrate : MonoBehaviour
@@ -11,15 +12,23 @@ public class Substrate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CreateCell(0, -2);
+        CreateCell(0, 0);
         //KillCell(cells[0]);
     }
 
     public void update()
     {
+        List<Cell> deadCells = new List<Cell>();
         foreach (Cell cell in cells)
         {
             cell.update();
+            if (cell.dead)
+                deadCells.Add(cell);
+        }
+        foreach (Cell cell in deadCells)
+        {
+            cells.Remove(cell);
+            cell.Destroy();
         }
     }
 
@@ -34,10 +43,9 @@ public class Substrate : MonoBehaviour
     void CreateCell(float x, float y)
     {
         GameObject cellObject = Instantiate(defaultCell, new Vector3(x, y, 0), new Quaternion());
-        cellObject.transform.localScale = new Vector3(cellObject.transform.localScale.x / radius, cellObject.transform.localScale.y / radius, 0);
-        Cell cell = cellObject.GetComponent<Cell>();
+        Cell cell = cellObject.AddComponent<Cell>();
+        cell.radius /= radius;
         cell.substrate = this;
-        cell.position = new Vector2(x, y);
         cells.Add(cell);
     }
 
