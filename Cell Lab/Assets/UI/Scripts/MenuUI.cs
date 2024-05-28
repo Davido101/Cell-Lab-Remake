2f2051416207605ac7465a1e7da0f6dee3a2cbdc
edy <lazareduard702@gmail.com>
2024-05-28 20:49:31 +0300

    Added Tab System to UI Library
    Added a Tab System to the game's UI Library, which will allow for further building of the Main Menu.

--- Cell Lab/Assets/UI/Scripts/MenuUI.cs
+++ Cell Lab/Assets/UI/Scripts/MenuUI.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class MenuUI : MonoBehaviour
 {
     public HorizontalList horizontalList;
+    public TabList tabList;
     public List<string> tabs = new List<string>() { "Gene Bank", "Experiments", "Challenges", "Settings", "About" };
     void Start()
     {
         horizontalList.AddOptions(tabs, 2);
+        tabList.SetTab("Challenges");
     }
 
     void Update()
     {
-        
+        tabList.SetTab(horizontalList.selectedOption);
     }
 }
---
+++ Cell Lab/Assets/UI/Scripts/TabList.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabList : MonoBehaviour
+{
+    public GameObject tabs;
+    public List<GameObject> tabList = new List<GameObject>();
+    public string currentTab;
+    public float glideSpeed;
+    GameObject currentTabGameObject;
+    void Start()
+    {
+        RectTransform tabsRect = tabs.GetComponent<RectTransform>();
+        int children = tabs.transform.childCount;
+        for (int i = 0; i < children; i++)
+        {
+            Transform child = tabs.transform.GetChild(i);
+            RectTransform rect = child.GetComponent<RectTransform>();
+            rect.anchoredPosition = new Vector2(tabsRect.sizeDelta.x * i, 0);
+            tabList.Add(child.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (currentTab != "")
+        {
+            RectTransform listRect = tabs.GetComponent<RectTransform>();
+            listRect.anchoredPosition = Vector2.Lerp(listRect.anchoredPosition, new Vector2(-currentTabGameObject.GetComponent<RectTransform>().anchoredPosition.x, listRect.anchoredPosition.y), Time.deltaTime * glideSpeed);
+        }
+    }
+
+    public void SetTab(string tab)
+    {
+        foreach (GameObject obj in tabList)
+        {
+            if (obj.name == tab)
+            {
+                currentTab = tab;
+                currentTabGameObject = obj;
+            }
+        }
+    }
+}
